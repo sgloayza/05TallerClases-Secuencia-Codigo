55945fcd34be62abefb6edc4f4137ca2b55a1d35
sgloayza <sgloayza@espol.edu.ec>
2020-11-18 18:54:34 -0500

    Sandra Loayza: creacion clases

---
+++ 05TallerClases-Secuencia-Codigo/05TallerClases-Secuencia-Codigo/src/pkg05tallerclases/secuencia/codigo/VehiculoDeCarga.java
@@ -0,0 +1,15 @@
+package pkg05tallerclases.secuencia.codigo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Intel
+ */
+public class VehiculoDeCarga {
+    
+}
